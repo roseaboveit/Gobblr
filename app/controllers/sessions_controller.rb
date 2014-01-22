0f191fc57b7638ddbe209dbf113052880485a76e
Davida Marion <davidamarion@gmail.com>
2014-01-22 14:33:42 -0800

    signin and signout functionality working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,6 +16,11 @@ class SessionsController < ApplicationController
     end
   end
 
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
   private
 
   def auth_hash
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-    # if @current_user
-    #   redirect_to home_path(@current_user.id)
-    # else
-    # end
+    if @current_user
+      redirect_to home_path(@current_user.id)
+    else
+    end
   end
 end
