6039e19ece0a6527b540543e583dbe8101fb846f
Whitney Levis <ms.whitney.levis@gmail.com>
2014-01-21 16:51:53 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gobblr::Application.config.secret_key_base = '91de5ec96ce66611ceb84768b5d6a04c4be0862e31b28c64427d6e58b3f08edcb8a7faab10ba3afb1cf2d6118359d0b4aa7d7944ba2af2706d714dfd4d44c0d3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gobblr::Application.config.session_store :cookie_store, key: '_Gobblr_session'
