dffa08446cb0e5675554e350653449395abcb27a
Davida Marion <davidamarion@gmail.com>
2014-01-26 15:25:53 -0800

    small cosmetic changes (i think)

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer # if Rails.env.development?
+  provider :twitter, ENV["TWITTER_CONSUMER_KEY"], ENV["TWITTER_CONSUMER_SECRET"]
 end
