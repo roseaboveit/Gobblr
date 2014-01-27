225feccc1d1ae27c58b355345414519758e455df
Whitney Levis <ms.whitney.levis@gmail.com>
2014-01-27 09:30:10 -0800

    Broken commit made to re-pull master

--- app/models/twitter_feed.rb
+++ app/models/twitter_feed.rb
@@ -2,10 +2,6 @@ class TwitterFeed < Feed
   require 'twitter'
   attr_accessor :client, :type
 
-  def initialize
-    super
-  end
-
   def self.search(twitter_search)
     @client = Twitter::REST::Client.new do |config|
       config.consumer_key    = ENV["TWITTER_CONSUMER_KEY"]
