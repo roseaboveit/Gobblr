6d0e5ca8b2eae01285dc88a92cd4b92150c9f7a0
Davida Marion <davidamarion@gmail.com>
2014-01-29 11:33:59 -0800

    stops making twitter crazy

--- app/models/twitter_feed.rb
+++ app/models/twitter_feed.rb
@@ -14,7 +14,10 @@ class TwitterFeed < Feed
       config.access_token_secret = secret
     end
     @twitter_user.home_timeline.each do |post|
-      @post = Post.create(author: post.user.screen_name, aurl: post.user.profile_image_url.to_s, published: post['created_at'], content: post.text, content_type: "text", url: post[:url].to_s,  feed_id: @feed.id)
+      if @feed.posts.where(url: post[:url].to_s).count == 1
+      else
+        @post = Post.create(author: post.user.screen_name, aurl: post.user.profile_image_url.to_s, published: post['created_at'], content: post.text, content_type: "text", url: post[:url].to_s,  feed_id: @feed.id)
+      end
     end
   end
 
