c1feff6c9cfdf26a81b44d2328109f93e2f70b2a
Elizabeth Uselton <elizabeth.uselton@gmail.com>
2014-01-30 11:33:07 -0800

    constantized things

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -34,15 +34,7 @@ class FeedsController < ApplicationController
     params.require(:feed).permit(:identifier, :user_id=>{})
   end
 
-#constantize
   def find_posts(feed_identifier, id)
-    # @feed.type.constantize.set_posts(feed_identifier, id)
-    if @feed.type == 'Feeder'
-      Feeder.set_posts(feed_identifier, id)
-    elsif @feed.type == 'Tumblr'
-      Tumblr.set_posts(feed_identifier, id)
-    elsif @feed.type == 'TwitterFeed'
-      TwitterFeed.set_posts(feed_identifier, id)
-    end
+    @feed.type.constantize.set_posts(feed_identifier, id)
   end
 end
