5a200b79d118484176fffbb6c8c4a7d7fbbb2c24
Davida Marion <davidamarion@gmail.com>
2014-01-31 15:19:28 -0800

    trying to create a rake task to update

--- app/models/twitter_feed.rb
+++ app/models/twitter_feed.rb
@@ -19,19 +19,24 @@ class TwitterFeed < Feed
   rescue Twitter::Error::TooManyRequests
   end
 
-  def self.search(twitter_search)
+  def self.app_authorization
     @client = Twitter::REST::Client.new do |config|
       config.consumer_key    = ENV["TWITTER_CONSUMER_KEY"]
       config.consumer_secret = ENV["TWITTER_CONSUMER_SECRET"]
       config.access_token = ENV["TWITTER_ACCESS_TOKEN"]
       config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
     end
+  end
+
+  def self.search(twitter_search)
+    app_authorization
     @client.user_search(twitter_search)
   rescue Twitter::Error::TooManyRequests
      {}
   end
 
   def self.set_posts(twitter_identifier, id)
+    app_authorization
     bunch_of_posts = @client.user_timeline(twitter_identifier)
     bunch_of_posts.each do |post|
       @post = Post.create(author: post.user.screen_name, aurl: post.user.profile_image_url.to_s, published: post['created_at'], feed_id: id, content: post.text, content_type: "text", url: post[:url].to_s)
