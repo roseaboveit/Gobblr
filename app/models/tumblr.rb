9ad1e66972d813a7fc41106689a3bdb1aaef69e1
Elizabeth Uselton <elizabeth.uselton@gmail.com>
2014-01-30 16:00:43 -0800

    fixed .tumblr.com bug

--- app/models/tumblr.rb
+++ app/models/tumblr.rb
@@ -1,15 +1,16 @@
 class Tumblr < Feed
 
   def self.search(tumblr_name)
+    tumblr_name.chomp!(".tumblr.com")
     walloftext = HTTParty.get("http://api.tumblr.com/v2/blog/#{tumblr_name}.tumblr.com/info?api_key=#{Figaro.env.tumblr_key}")
     if walloftext['response'].length != 0
-      { title: walloftext['response']['blog']['title'], 
-                  author: walloftext['response']['blog']['name'],
-                  url: walloftext['response']['blog']['url'],
-                  post_count: walloftext['response']['blog']['posts'],
-                  time: walloftext['response']['blog']['updated'],
-                  description: walloftext['response']['blog']['description'],
-                  avatar: "http://api.tumblr.com/v2/blog/#{tumblr_name}.tumblr.com/avatar/96" }
+      { title:       walloftext['response']['blog']['title'], 
+        author:      walloftext['response']['blog']['name'],
+        url:         walloftext['response']['blog']['url'],
+        post_count:  walloftext['response']['blog']['posts'],
+        time:        walloftext['response']['blog']['updated'],
+        description: walloftext['response']['blog']['description'],
+        avatar:      "http://api.tumblr.com/v2/blog/#{tumblr_name}.tumblr.com/avatar/96" }
     else
       0
     end
