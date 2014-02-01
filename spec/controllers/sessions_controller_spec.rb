9d7db53b13a9b52f3813cefcb2e3cccdcb3d881f
Davida Marion <davidamarion@gmail.com>
2014-02-01 14:48:20 -0800

    some omniauth spec workin

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -8,25 +8,24 @@ describe SessionsController do
 
         # I commented these out because apparently we don't need em? And they don't work. So, maybe if we have time.
 
-        # it "redirects to home page" do
-        #   get :create, provider: :twitter
-        #   expect(response).to be_redirect
-        # end
+        it "redirects to home page" do
+          get :create, provider: :twitter
+          expect(response).to be_redirect
+        end
 
-        # it "creates a user" do
-        #   expect { get :create, provider: :twitter }.to change(User, :count).by(1)
-        # end
+        it "creates a user" do
+          expect { get :create, provider: :twitter }.to change(User, :count).by(1)
+        end
 
+        it "assigns the @current_user var" do
+          get :create, provider: :twitter
+          expect(assigns(:current_user)).to be_an_instance_of User
+        end
 
-        # it "assigns the @current_user var" do
-        #   get :create, provider: :twitter
-        #   expect(assigns(:current_user)).to be_an_instance_of User
-        # end
-
-        # it "assigns the session[:user_id]" do
-        #   get :create, provider: :twitter
-        #   expect(session[:user_id]).to eq assigns(:current_user).id
-        # end
+        it "assigns the session[:user_id]" do
+          get :create, provider: :twitter
+          expect(session[:user_id]).to eq assigns(:current_user).id
+        end
 
       end
 
@@ -50,7 +49,6 @@ describe SessionsController do
       #   it "redirect to home with flash error" do
       #     get :create, provider: :twitter
       #     expect(response).to redirect_to root_path
-      #     expect(flash[:notice]).to include "Failed to authenticate"
       #   end
       # end
     end
