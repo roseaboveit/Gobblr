bca760df0f664168f08f9637367d68c8fde15dc6
Davida Marion <davidamarion@gmail.com>
2014-02-01 11:15:04 -0800

    rake task works locally

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -5,5 +5,13 @@ class Feed < ActiveRecord::Base
   validates :type, presence: true
   validates :identifier, presence: true, uniqueness: true
 
-  
+  def self.update
+    Feed.all.each do |feed|
+      if feed.type == TwitterFeed && feed.identifier.include?(/_help_line/)
+      else
+      feed.type.constantize.set_posts(feed.identifier, feed.id)
+      end
+    end
+  end
+
 end
